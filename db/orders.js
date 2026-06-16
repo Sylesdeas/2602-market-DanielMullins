de39858cdfe8acc38b6a9949ca5d83c1d70be9d6
Daniel Mullins <sylesdeas27@gmail.com>
2026-06-15 21:42:57 -0400

    created the database queries

---
+++ db/orders.js
@@ -0,0 +1,54 @@
+import db from "#db/client";
+
+export async function createOrder({ date, note, userId }) {
+  const { rows } = await db.query(
+    `
+      INSERT INTO orders (date, note, user_id) VALUES ($1, $2, $3) RETURNING *;
+    `,
+    [date, note || null, userId],
+  );
+  return rows[0];
+}
+
+export async function getOrdersByUserId(userId) {
+  const { rows } = await db.query(
+    `
+      SELECT * FROM orders WHERE user_id = $1 ORDER BY id;
+    `,
+    [userId],
+  );
+  return rows;
+}
+
+export async function getOrderById(id) {
+  const { rows } = await db.query(
+    `
+      SELECT * FROM orders WHERE id = $1;
+    `,
+    [id],
+  );
+  return rows[0];
+}
+
+export async function addProductToOrder(orderId, productId, quantity) {
+  const { rows } = await db.query(
+    `
+      INSERT INTO orders_products (order_id, product_id, quantity) VALUES ($1, $2, $3) RETURNING *;
+    `,
+    [orderId, productId, quantity],
+  );
+  return rows[0];
+}
+
+export async function getProductsByOrderId(orderId) {
+  const { rows } = await db.query(
+    `
+      SELECT products.*, orders_products.quantity FROM products
+      JOIN orders_products ON products.id = orders_products.product_id
+      WHERE orders_products.order_id = $1;
+      ORDER BY products.id;
+    `,
+    [orderId],
+  );
+  return rows;
+}
---
+++ db/users.js
@@ -0,0 +1,19 @@
+import bcrypt from "bcrypt";
+import db from "#db/client";
+
+export async function createUser(username, password) {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const { rows } = await db.query(
+    "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id, username",
+    [username, hashedPassword],
+  );
+  return rows[0];
+}
+
+export async function getUserByUsername(username) {
+  const { rows } = await db.query(
+    "SELECT id, username, password FROM users WHERE username = $1",
+    [username],
+  );
+  return rows[0];
+}
